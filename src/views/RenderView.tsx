c3f265f316dbb6087e0295f3788747302b72ed34
Vince Au <vinceau09@gmail.com>
2020-11-10 17:38:55 +1100

    Add powered by logo

--- src/views/RenderView.tsx
+++ src/views/RenderView.tsx
@@ -1,13 +1,39 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+
 import React from "react";
 import { useQuery } from "../lib/hooks";
+
+import poweredByImage from "../styles/images/powered-by.png";
 
 export const RenderView: React.FC = (props) => {
   const [key, setKey] = React.useState("");
   const [val, setVal] = React.useState("");
   const { paramMap, setQuery } = useQuery();
   return (
-    <div>
-      <h3>hello world</h3>
+    <div
+      css={css`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      `}
+    >
+      <h1
+        css={css`
+          text-transform: uppercase;
+          font-weight: 800;
+          font-size: 60px;
+          margin: 20px;
+        `}
+      >
+        Post Match Stats
+      </h1>
+      <img
+        css={css`
+          max-height: 50px;
+        `}
+        src={poweredByImage}
+      />
       <label>key</label>
       <input value={key} onChange={(e) => setKey(e.target.value)} />
       <label>val</label>
