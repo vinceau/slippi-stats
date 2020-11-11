64b484bcffc81f292bb7f5f1b35b862b965b4ef1
vinceau <vinceau@users.noreply.github.com>
2020-11-11 04:40:41 +0000

    Automated deployment: Wed Nov 11 04:40:41 UTC 2020 42c6f3022396aeebe6ec326ef43ec0dd26a5121a

---
+++ precache-manifest.3f8f613e6321074b634a7b99c4a74fa1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde53c84dbc7a9752ca1b9156fee2cc1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c52646ed5a43cd85b7b7",
+    "url": "./static/css/main.47fe427c.chunk.css"
+  },
+  {
+    "revision": "fc73ee116c0e41adebf1",
+    "url": "./static/js/2.0fa0fa74.chunk.js"
+  },
+  {
+    "revision": "919b1ffbc0489d06a25051ac53821d16",
+    "url": "./static/js/2.0fa0fa74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c52646ed5a43cd85b7b7",
+    "url": "./static/js/main.bfb0869e.chunk.js"
+  },
+  {
+    "revision": "c44ea96ee4d6559c4d40",
+    "url": "./static/js/runtime-main.7f0cce33.js"
+  },
+  {
+    "revision": "61867ea86d6d6f232e4dcfac1f6c6f76",
+    "url": "./static/media/powered-by.61867ea8.png"
+  }
+]);
