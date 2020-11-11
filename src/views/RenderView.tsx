46c6ba54f0b963968a6897d2c81d3fa0089abfed
Vince Au <vinceau09@gmail.com>
2020-11-11 16:25:29 +1100

    Add header border

--- src/views/RenderView.tsx
+++ src/views/RenderView.tsx
@@ -1,9 +1,32 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
+import styled from "@emotion/styled";
 import React from "react";
 
 import { StatDisplay } from "../containers/StatDisplay";
 import poweredByImage from "../styles/images/powered-by.png";
+
+const Header = styled.h1`
+  text-transform: uppercase;
+  font-weight: 800;
+  font-size: 6rem;
+  margin: 2rem;
+  padding-bottom: 1.5rem;
+  position: relative;
+
+  &::before {
+    z-index: -1;
+    content: "";
+    display: block;
+    background-image: linear-gradient(to right, transparent, rgba(255, 255, 255, 0.2), transparent);
+    height: 0.1rem;
+    position: absolute;
+    width: 80%;
+    margin-left: 50%;
+    transform: translateX(-50%);
+    bottom: 0;
+  }
+`;
 
 export const RenderView: React.FC = (props) => {
   return (
@@ -15,20 +38,12 @@ export const RenderView: React.FC = (props) => {
         align-items: center;
       `}
     >
-      <h1
-        css={css`
-          text-transform: uppercase;
-          font-weight: 800;
-          font-size: 6rem;
-          margin: 2rem;
-        `}
-      >
-        Post Match Stats
-      </h1>
+      <Header>Post Match Stats</Header>
       <img
         alt="Powered by Slippi"
         css={css`
           max-height: 5rem;
+          margin-bottom: 3rem;
         `}
         src={poweredByImage}
       />
