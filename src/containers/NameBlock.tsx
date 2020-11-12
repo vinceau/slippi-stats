d67d4b5f5f77ccbd0c90d96d2ecba1f3e5a0214a
Vince Au <vinceau09@gmail.com>
2020-11-12 23:52:32 +1100

    Add NameBlock container

---
+++ src/containers/NameBlock.tsx
@@ -0,0 +1,50 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+import styled from "@emotion/styled";
+import { useParam } from "lib/hooks";
+
+import React from "react";
+import { TextBlock } from "../components/TextBlock";
+
+const Name = styled.div`
+  font-size: 150%;
+  font-weight: 800;
+`;
+
+const Subtitle = styled.div<{
+  show?: boolean;
+}>`
+  margin-top: -0.3rem;
+  margin-left: auto;
+  margin-right: auto;
+  width: 80%;
+`;
+
+export interface NameBlockProps {
+  nameParam: string;
+  defaultName: string;
+  subtitleParam: string;
+  primaryColor: string;
+  secondaryColor: string;
+}
+
+export const NameBlock: React.FC<NameBlockProps> = ({
+  nameParam,
+  defaultName,
+  subtitleParam,
+  primaryColor,
+  secondaryColor,
+}) => {
+  const [name, setName] = useParam(nameParam, defaultName);
+  const [sub, setSub] = useParam(subtitleParam);
+  return (
+    <div>
+      <Name>
+        <TextBlock value={name} onEdit={setName} color="white" backgroundColor={primaryColor} />
+      </Name>
+      <Subtitle>
+        <TextBlock value={sub} optional={true} onEdit={setSub} color={secondaryColor} backgroundColor="white" />
+      </Subtitle>
+    </div>
+  );
+};
--- src/containers/RenderDisplay.tsx
+++ src/containers/RenderDisplay.tsx
@@ -1,13 +1,13 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 import styled from "@emotion/styled";
-import { TextBlock } from "components/TextBlock";
 import { useParam } from "lib/hooks";
 import { PortColor } from "lib/portColor";
 import React from "react";
 import { Theme } from "styles/theme";
 
 import { CharDisplay } from "./CharDisplay";
+import { NameBlock } from "./NameBlock";
 import { StatDisplay } from "./StatDisplay";
 
 const Outer = styled.div`
@@ -17,31 +17,9 @@ const Outer = styled.div`
   grid-template-columns: 20% 60% 20%;
 `;
 
-const NameBlock = styled.div`
-  width: 20%;
-`;
-
-const Name = styled.div`
-  font-size: 150%;
-  font-weight: 800;
-`;
-
-const Subtitle = styled.div<{
-  show?: boolean;
-}>`
-  margin-top: -0.3rem;
-  margin-left: auto;
-  margin-right: auto;
-  width: 80%;
-`;
-
-export const RenderDisplay: React.FC<Theme> = ({ primaryColor, secondaryColor }) => {
+export const RenderDisplay: React.FC<Theme> = (theme) => {
   const [leftColor] = useParam("leftColor", PortColor.P1);
   const [rightColor] = useParam("rightColor", PortColor.P2);
-  const [name1, setName1] = useParam("name1", "HELLO");
-  const [name2, setName2] = useParam("name2", "WORLD");
-  const [sub1, setSub1] = useParam("sub1", "");
-  const [sub2, setSub2] = useParam("sub2", "");
   return (
     <Outer>
       <div
@@ -52,12 +30,7 @@ export const RenderDisplay: React.FC<Theme> = ({ primaryColor, secondaryColor })
       >
         <CharDisplay theme={leftColor} charParam="char1" colorParam="color1" align="right" />
       </div>
-      <StatDisplay
-        primaryColor={primaryColor}
-        secondaryColor={secondaryColor}
-        leftColor={leftColor}
-        rightColor={rightColor}
-      />
+      <StatDisplay leftColor={leftColor} rightColor={rightColor} {...theme} />
       <div
         css={css`
           width: 100%;
@@ -82,22 +55,20 @@ export const RenderDisplay: React.FC<Theme> = ({ primaryColor, secondaryColor })
             align-items: center;
           `}
         >
-          <NameBlock>
-            <Name>
-              <TextBlock value={name1} onEdit={setName1} color="white" backgroundColor={primaryColor} />
-            </Name>
-            <Subtitle>
-              <TextBlock value={sub1} optional={true} onEdit={setSub1} color={secondaryColor} backgroundColor="white" />
-            </Subtitle>
-          </NameBlock>
-          <NameBlock>
-            <Name>
-              <TextBlock value={name2} onEdit={setName2} color="white" backgroundColor={primaryColor} />
-            </Name>
-            <Subtitle>
-              <TextBlock value={sub2} optional={true} onEdit={setSub2} color={secondaryColor} backgroundColor="white" />
-            </Subtitle>
-          </NameBlock>
+          <div
+            css={css`
+              width: 20%;
+            `}
+          >
+            <NameBlock nameParam="name1" subtitleParam="sub1" defaultName="Player" {...theme} />
+          </div>
+          <div
+            css={css`
+              width: 20%;
+            `}
+          >
+            <NameBlock nameParam="name2" subtitleParam="sub2" defaultName="Player" {...theme} />
+          </div>
         </div>
       </div>
     </Outer>
