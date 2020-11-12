769350378d05863aa0bcb5090cbd5685b0583bdc
Vince Au <vinceau09@gmail.com>
2020-11-12 21:44:15 +1100

    Increase stage blur

--- src/components/GameResult/StageTimer.tsx
+++ src/components/GameResult/StageTimer.tsx
@@ -19,28 +19,29 @@ const Text = styled.span`
 const Outer = styled.div<{
   src: string;
 }>`
-border: solid 1px rgba(255, 255, 255, 0.3);
-z-index: 1;
-position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  height: 9rem;
+  width: 100%;
+  border: solid 1px rgba(255, 255, 255, 0.3);
+  z-index: 1;
+
+  &::after {
+    content: ' ';
+    height: 100%;
     width: 100%;
-    height: 9rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    &::after {
-      z-index: -1;
-        content: ' ';
-        position: absolute;
-        top: 0;
-        left: 0;
-        height: 100%;
-        width: 100%;
-  background-image: url("${(p) => p.src}");
-  background-repeat: no-repeat;
-  background-size: 105% auto;
-  background-position: center;
-  filter: blur(1px) brightness(30%);
-    }
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-image: url("${(p) => p.src}");
+    background-repeat: no-repeat;
+    background-size: 105% auto;
+    background-position: center;
+    filter: blur(2px) brightness(30%);
+    z-index: -1;
+  }
 `;
 
 export const StageTimer: React.FC<StageTimerProps> = ({ stageId, duration }) => {
