3293aff87d99c963ad7b8a2f58adf954855f76a2
Vince Au <vinceau09@gmail.com>
2020-11-13 02:14:18 +1100

    Fix character alignments for Sheik and Bowser

--- src/containers/CharDisplay/index.tsx
+++ src/containers/CharDisplay/index.tsx
@@ -11,12 +11,31 @@ export interface CharDisplayProps {
   theme: string;
 }
 
+// Some characters have their faces to the right so it get cut off.
+// e.g. Sheik and Bowser
+// So make sure we return the desired alignment
+const FLIP_RIGHT_CHARS = ["19", "5"];
+
+function getAlignment(facing: Side, char: string, defaultAlignment: Side): Side {
+  if (facing === "left") {
+    return defaultAlignment;
+  }
+
+  // We so far only care about the right hand side of the VS-screen
+  if (FLIP_RIGHT_CHARS.includes(char)) {
+    return "right";
+  }
+
+  return defaultAlignment;
+}
+
 export const CharDisplay: React.FC<CharDisplayProps> = (props) => {
   const { colorParam, charParam, align, theme } = props;
   const [color] = useParam(colorParam);
   const [char] = useParam(charParam);
   const facing: Side = align === "left" ? "right" : "left";
   const imgSrc = getCharacterVSScreen(facing, char, color);
+  const alignment = getAlignment(facing, char, align);
   return (
     <div
       css={css`
@@ -32,7 +51,7 @@ export const CharDisplay: React.FC<CharDisplayProps> = (props) => {
           background-image: url("${imgSrc}");
           background-repeat: no-repeat;
           background-size: cover;
-          background-position: top ${align};
+          background-position: top ${alignment};
         }
       `}
     ></div>
