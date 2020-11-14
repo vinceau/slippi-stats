80b04a3c139fcafc7c9caebb0f4807d6f56fa973
Vince Au <vinceau09@gmail.com>
2020-11-14 20:02:17 +1100

    Move ColorPickers into a single folder

--- src/containers/Options/Panel.tsx
+++ src/containers/Options/Panel.tsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */ import { css, jsx } from "@emotion/core";
-import { DualColorPicker } from "components/DualColorPicker";
+import { DualColorPicker } from "components/ColorPicker";
 import { useParam } from "lib/hooks";
 import React from "react";
 
