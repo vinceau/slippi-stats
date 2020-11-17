3525d6f706ad39d5b0ae0b78f3bb5ea462df3cfd
Vince Au <vinceau09@gmail.com>
2020-11-17 12:13:30 +1100

    Set homepage field in package.json rather than in code

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,7 @@ import { RenderView } from "./views/RenderView";
 const App: React.FC = () => {
   return (
     <AppProvider>
-      {/* We have to define the basename here as the Github pages URL segment or the SPA won't work. */}
-      <Router basename="/slippi-stats">
+      <Router basename={process.env.PUBLIC_URL}>
         <Route exact path="/" component={MainView} />
         <Route path="/render" component={RenderView} />
         <Route path="/random" component={RandomView} />
