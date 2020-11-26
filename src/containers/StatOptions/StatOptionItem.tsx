cb45d59682cd6a916a394ca657d64243cf4ad7c2
Vince Au <vinceau09@gmail.com>
2020-11-26 20:42:58 +1100

    Update StatOptionItem styles

--- src/containers/StatOptions/StatOptionItem.tsx
+++ src/containers/StatOptions/StatOptionItem.tsx
@@ -4,14 +4,35 @@ import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 
 const Outer = styled.div<{
-  isDragging: boolean;
+  checked?: boolean;
+  isDragging?: boolean;
 }>`
+  opacity: ${(p) => (p.checked ? "1" : "0.5")};
+  background-color: ${(p) => (p.isDragging ? "rgba(0, 0, 0, 0.6)" : "rgba(0, 0, 0, 0.3)")};
+  color: white;
+  padding: 0.5rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 0.5rem;
+  font-weight: ${(p) => (p.checked ? "600" : "100")};
   ${(p) =>
     p.isDragging
       ? `
     box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+    opacity: 1;
 `
       : ""}
+
+  .title {
+    cursor: pointer;
+    margin-left: 1rem;
+    user-select: none;
+  }
+  .handle {
+    display: flex;
+  }
 `;
 
 export const StatOptionItem: React.FC<{
@@ -21,18 +42,26 @@ export const StatOptionItem: React.FC<{
   onChange: (checked: boolean) => void;
 }> = (props) => {
   const stat = (STAT_DEFINITIONS as any)[props.id];
+  const toggle = () => props.onChange(!props.checked);
   return (
     <Draggable draggableId={props.id} index={props.index}>
       {(provided, snapshot) => (
         <Outer
           {...provided.draggableProps}
-          {...provided.dragHandleProps}
           ref={provided.innerRef}
           isDragging={snapshot.isDragging}
+          checked={props.checked}
         >
           <div>
-            <input type="checkbox" checked={props.checked} onChange={() => props.onChange(!props.checked)} />
-            {stat.name}
+            <input type="checkbox" checked={props.checked} onChange={toggle} />
+            <span className="title" onClick={toggle}>
+              {stat.name}
+            </span>
+          </div>
+          <div className="handle" {...provided.dragHandleProps}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+              <path d="M20 9H4v2h16V9zM4 15h16v-2H4v2z" fill="#ffffff" />
+            </svg>
           </div>
         </Outer>
       )}
