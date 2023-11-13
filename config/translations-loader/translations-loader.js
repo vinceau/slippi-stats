c3673fa0959ae5cc7a6ad0234b5f802390763f75
Vince Au <vinceau09@gmail.com>
2023-11-13 19:30:01 +1100

    i think it kinda works

--- config/translations-loader/translations-loader.js
+++ config/translations-loader/translations-loader.js
@@ -14,20 +14,57 @@ module.exports = function (source) {
   // so you could see info about the emitted asset.
   const assetInfo = { sourceFilename: filename };
 
+  const sourceFile = ts.createSourceFile(this.resourcePath, source, ts.ScriptTarget.Latest);
+
+  const transformerFactory = (context) => {
+    return (rootNode) => {
+      function visit(node) {
+        node = ts.visitEachChild(node, visit, context);
+        if (ts.isReturnStatement(node)) {
+          // return ts.createReturnStatement();
+          return ts.factory.createReturnStatement(ts.createIdentifier('i18next.t("something")'));
+        }
+
+        // if (ts.isIdentifier(node)) {
+        //     return ts.createIdentifier(node.text + "suffix");
+        // } else {
+        //     return node;
+        // }
+        return node;
+      }
+
+      return ts.visitNode(rootNode, visit);
+    };
+  };
+
+  const transformationResult = ts.transform(sourceFile, [transformerFactory]);
+
+  // ts.factory.createReturnStatement
+
+  const transformedSourceFile = transformationResult.transformed[0];
+  const printer = ts.createPrinter();
+
+  const result = printer.printNode(ts.EmitHint.Unspecified, transformedSourceFile, undefined);
+
+  console.log(`\n\n\n>>> end processing ${this.resourcePath} <<<\n\n\n`);
+
+  const newSource = result; // source;
+
+  console.log(newSource);
+
   // Finally, emit the imported audio file's "source"
   // in the webpack's build directory using a built-in
   // "emitFile" method.
-  this.emitFile(filename, source, null, assetInfo);
+  this.emitFile(filename, newSource, null, assetInfo);
 
   // const result = ts.transpile(source, { module: ts.ModuleKind.CommonJS });
   // console.log({ result });
 
-  const sourceFile = ts.createSourceFile(this.resourcePath, source, ts.ScriptTarget.Latest);
-  console.log(sourceFile.getChildren());
+  // const sourceFile = ts.createSourceFile(this.resourcePath, source, ts.ScriptTarget.Latest);
+  // console.log(sourceFile.getChildren());
   // printRecursiveFrom(sourceFile, 0, sourceFile);
 
-  console.log(`\n\n\n>>> end processing ${this.resourcePath} <<<\n\n\n`);
-  return source;
+  return newSource;
 };
 
 // function findExports(node) {
