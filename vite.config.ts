ed42ccb87d5bc61efdba6489bf53379748ce077b
Vince Au <vinceau09@gmail.com>
2025-11-17 16:16:31 +1100

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,60 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/slippi-stats/",
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
       babel: {
         plugins: ["@emotion/babel-plugin"],
+      },
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "google-fonts-cache",
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+              },
+            },
+          },
+        ],
+      },
+      includeAssets: ["favicon.ico", "logo192.png", "logo512.png"],
+      manifest: {
+        name: "Generate pretty Summit-style Slippi set stats",
+        short_name: "Slippi Stats Graphic Generator",
+        description: "Instantly compute Slippi stats and beautifully render the results.",
+        theme_color: "#286163",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "favicon.ico",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/x-icon",
+          },
+          {
+            src: "logo192.png",
+            type: "image/png",
+            sizes: "192x192",
+          },
+          {
+            src: "logo512.png",
+            type: "image/png",
+            sizes: "512x512",
+          },
+        ],
       },
     }),
   ],
@@ -18,6 +65,6 @@ export default defineConfig({
   },
   define: {
     "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development"),
-    "process.env.PUBLIC_URL": JSON.stringify(process.env.PUBLIC_URL || ""),
+    "process.env.PUBLIC_URL": JSON.stringify(process.env.PUBLIC_URL || "/slippi-stats"),
   },
 });
