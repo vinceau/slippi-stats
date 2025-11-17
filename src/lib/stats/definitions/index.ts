0f2ef8e075d524c9fa6a109877f80aa7fdadc731
Vince Au <vinceau09@gmail.com>
2025-11-17 15:52:42 +1100

    fix things

---
+++ src/lib/stats/definitions/index.ts
@@ -0,0 +1,206 @@
+/* eslint-disable */
+// @ts-nocheck
+
+/*
+ * Taken from: https://github.com/project-slippi/slippi-set-stats/blob/master/main.js
+ */
+
+import _ from "lodash";
+
+export * from "./firstBlood";
+export * from "./lCancelAccuracy";
+export * from "./neutralOpenerMoves";
+export * from "./killMoves";
+export * from "./selfDestructs";
+export * from "./highestDamagePunish";
+
+export const openingsPerKill = {
+  name: "Openings / Kill",
+  type: "number",
+  betterDirection: "lower",
+  recommendedRounding: 1,
+  calculate(games: any, playerIndex: any) {
+    return genOverallRatioStat(games, playerIndex, "openingsPerKill", this.recommendedRounding);
+  },
+};
+
+export const damagePerOpening = {
+  name: "Damage / Opening",
+  type: "number",
+  betterDirection: "higher",
+  recommendedRounding: 1,
+  calculate(games: any, playerIndex: any) {
+    return genOverallRatioStat(games, playerIndex, "damagePerOpening", this.recommendedRounding);
+  },
+};
+
+export const neutralWins = {
+  name: "Neutral Wins",
+  type: "number",
+  betterDirection: "higher",
+  recommendedRounding: 0,
+  calculate(games: any, playerIndex: any) {
+    return genOverallRatioStat(games, playerIndex, "neutralWinRatio", this.recommendedRounding, "count");
+  },
+};
+
+export const earlyKills = {
+  name: "Earliest Kill",
+  type: "number",
+  betterDirection: "lower",
+  recommendedRounding: 1,
+  calculate(games: any, playerIndex: any) {
+    const oppStocks = _.flatMap(games, (game: any) => {
+      const stocks = _.get(game, ["stats", "stocks"]) || [];
+      return _.filter(stocks, (stock: any) => {
+        const isOpp = stock.playerIndex !== playerIndex;
+        const hasEndPercent = stock.endPercent !== null;
+        return isOpp && hasEndPercent;
+      });
+    });
+
+    const orderedOppStocks = _.orderBy(oppStocks, ["endPercent"], ["asc"]);
+    const earliestKillStock = _.first(orderedOppStocks);
+    const simple = {
+      text: "N/A",
+      number: null,
+    };
+
+    if (earliestKillStock) {
+      simple.number = earliestKillStock.endPercent;
+      simple.text = simple.number.toFixed(this.recommendedRounding);
+    }
+
+    return {
+      result: _.take(orderedOppStocks, 5),
+      simple: simple,
+    };
+  },
+};
+
+export const lateDeaths = {
+  name: "Latest Death",
+  type: "number",
+  betterDirection: "higher",
+  recommendedRounding: 0,
+  calculate(games: any, playerIndex: any) {
+    const playerStocks = _.flatMap(games, (game: any) => {
+      const stocks = _.get(game, ["stats", "stocks"]) || [];
+      return _.filter(stocks, (stock: any) => {
+        const isPlayer = stock.playerIndex === playerIndex;
+        const hasEndPercent = stock.endPercent !== null;
+        return isPlayer && hasEndPercent;
+      });
+    });
+
+    const orderedPlayerStocks = _.orderBy(playerStocks, ["endPercent"], ["desc"]);
+    const latestDeathStock = _.first(orderedPlayerStocks);
+    const simple = {
+      text: "N/A",
+      number: null,
+    };
+
+    if (latestDeathStock) {
+      simple.number = latestDeathStock.endPercent;
+      simple.text = simple.number.toFixed(this.recommendedRounding);
+    }
+
+    return {
+      result: _.take(orderedPlayerStocks, 5),
+      simple: simple,
+    };
+  },
+};
+
+export const inputsPerMinute = {
+  name: "Inputs / Minute",
+  type: "number",
+  betterDirection: "higher",
+  recommendedRounding: 0,
+  calculate(games: any, playerIndex: any) {
+    return genOverallRatioStat(games, playerIndex, "inputsPerMinute", this.recommendedRounding);
+  },
+};
+
+export const averageKillPercent = {
+  name: "Average Kill Percent",
+  type: "number",
+  betterDirection: "lower",
+  recommendedRounding: 0,
+  calculate(games: any, playerIndex: any) {
+    const oppStocks = _.flatMap(games, (game: any) => {
+      const stocks = _.get(game, ["stats", "stocks"]) || [];
+      return _.filter(stocks, (stock: any) => {
+        const isOpp = stock.playerIndex !== playerIndex;
+        const hasEndPercent = stock.endPercent !== null;
+        return isOpp && hasEndPercent;
+      });
+    });
+
+    const result = {
+      total: oppStocks.length,
+      count: _.sumBy(oppStocks, "endPercent") || 0,
+    };
+
+    result.ratio = result.total ? result.count / result.total : null;
+
+    return {
+      result: result,
+      simple: genSimpleFromRatio(result, this.recommendedRounding),
+    };
+  },
+};
+
+export const damageDone = {
+  name: "Total Damage Done",
+  type: "number",
+  betterDirection: "higher",
+  recommendedRounding: 0,
+  calculate(games: any, playerIndex: any) {
+    return genOverallRatioStat(games, playerIndex, "damagePerOpening", this.recommendedRounding, "count");
+  },
+};
+
+function genOverallRatioStat(games: any, playerIndex: any, field: any, fixedNum: any, type = "ratio") {
+  const statRatios = _.map(games, (game: any) => {
+    const overallStats = _.get(game, ["stats", "overall"]);
+    const overallStatsByPlayer = _.keyBy(overallStats, "playerIndex");
+    const overallStatsForPlayer = overallStatsByPlayer[playerIndex];
+    return overallStatsForPlayer[field];
+  });
+
+  const avg = averageRatios(statRatios);
+  const simple = genSimpleFromRatio(avg, fixedNum, type);
+
+  return {
+    result: avg,
+    simple: simple,
+  };
+}
+
+function averageRatios(ratios: any) {
+  const result: any = {};
+
+  result.count = _.sumBy(ratios, "count") || 0;
+  result.total = _.sumBy(ratios, "total") || 0;
+  result.ratio = result.total ? result.count / result.total : null;
+
+  return result;
+}
+
+function genSimpleFromRatio(ratio: any, fixedNum: any, type = "ratio") {
+  const result: any = {};
+
+  switch (type) {
+    case "ratio":
+      result.number = ratio.ratio;
+      result.text = ratio.ratio !== null ? ratio.ratio.toFixed(fixedNum) : "N/A";
+      break;
+    case "count":
+      result.number = ratio.count;
+      result.text = ratio.count.toFixed(fixedNum);
+      break;
+  }
+
+  return result;
+}
---
+++ src/lib/stats/filterGames.ts
@@ -0,0 +1,52 @@
+/* eslint-disable */
+// @ts-nocheck
+
+/*
+ * Taken from: https://github.com/project-slippi/slippi-set-stats/blob/master/main.js
+ */
+
+import _ from "lodash";
+
+export function filterGames(games: any) {
+  // console.log(games);
+  const gamesByIsSingles = _.groupBy(games, (game: any) => {
+    const numberOfPlayers = game.settings.players.length;
+    return numberOfPlayers === 2;
+  });
+
+  const nonSinglesGames = _.get(gamesByIsSingles, false) || [];
+  if (_.some(nonSinglesGames)) {
+    console.log("The following games have been excluded because they are not singles games:");
+    _.forEach(nonSinglesGames, (game: any) => {
+      console.log(game.filePath);
+    });
+    console.log();
+  }
+
+  const singlesGames = _.get(gamesByIsSingles, true) || [];
+  const gamesByPorts = _.chain(singlesGames)
+    .groupBy((game: any) => {
+      const ports = _.map(game.settings.players, "port");
+      return _.join(ports, "-");
+    })
+    .orderBy(["length"], ["desc"])
+    .value();
+
+  const gamesWithSamePorts = gamesByPorts.shift();
+  if (_.some(gamesByPorts)) {
+    console.log("The following games have been excluded because the player ports differ:");
+    const flatGames = _.flatten(gamesByPorts);
+    _.forEach(flatGames, (game: any) => {
+      console.log(game.filePath);
+    });
+    console.log();
+  }
+
+  if (_.isEmpty(gamesWithSamePorts)) {
+    throw new Error("There were no valid games found to compute stats from.");
+  }
+
+  console.log(`Including ${gamesWithSamePorts.length} games for stat calculation...`);
+
+  return gamesWithSamePorts;
+}
